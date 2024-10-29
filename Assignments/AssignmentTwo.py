cbb12bc1e3fab6a293cfea82c48a0b5f32e00ccc
Jonathan753522 <753522@pdsb.net>
2024-10-29 10:51:39 -0400

    Update and rename AssignmentTwo to AssignmentTwo.py

---
+++ Assignments/AssignmentTwo.py
@@ -0,0 +1,63 @@
+"""
+   Author : Jonathan Lee
+   Revison date : 29 October 2024
+   Program : Yearbook Photo Optimzier
+   Description : What ever number you input it finds the optimal perimeter
+   VARIABLE DICTIONARY :
+     Fullname (String) = person's name
+     height (double) = person's height
+     Lastname (String) = string object for entering the last-names
+     Firstname (String) = string object for entering the *first-names
+     B (String) = a variable for ignoring the first line in the file
+     sum (double) = variable for the total sum of the heights
+     avg (double) = variable for the average of all heights
+     count (int) = the number of people averaged
+"""
+
+import math
+
+#Functions
+
+def PhotoFactors(N):
+    Photonum = []
+    mnum = math.floor(math.sqrt(N))
+    for Xaxis in range(1,mnum + 1): 
+        if N % Xaxis == 0:  
+            Photonum.append(Xaxis)
+  
+    return Photonum
+
+#Calculates Perimeter
+def perim(N):
+    Factor = PhotoFactors(N)
+    Xaxis = 1
+    for num in Factor:
+        if num > Xaxis:
+            Xaxis = num 
+    Yaxis = N / Xaxis
+    perimeter = 2 * (Xaxis + Yaxis)
+    print("Minimum perimeter is %d with dimensions: %d x %d" % (perimeter, Xaxis, Yaxis))
+
+#Printing Statments/Loop
+print("Welcome to the school yearbook program!")
+
+done = False
+
+#While Loop
+
+while (done != True):
+    try:
+        userinp = (input("Please Input A Number Of Photographs: "))
+        if (userinp.lower() == "done") or (userinp == "0"):
+            print("Goodbye!")
+            done = True
+            break
+        if not isinstance(userinp, int):
+            photooutput = int(userinp)
+        if (photooutput < 0):
+            print("%d is not a valid number of photos" % (photooutput))
+            print("Please input a positive number")
+        elif photooutput > 0:
+            erim(photooutput)
+    except:
+        print("%s is not a valid number of photos" % (userinp))
