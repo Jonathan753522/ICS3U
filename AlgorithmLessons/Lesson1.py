d24fcd33ef5130a70bc90f7b55a02b4360205a51
Jonathan753522 <753522@pdsb.net>
2024-11-13 10:51:10 -0500

    Create Lesson1.py

---
+++ AlgorithmLessons/Lesson1.py
@@ -0,0 +1,18 @@
+#Part 1
+import turtle
+
+t = turtle.Turtle()
+
+
+for i in range(4):
+    t.forward(100)
+    t.left(90)
+
+#Part 2
+import turtle
+
+t = turtle.Turtle()
+
+for i in range(60):
+  t.forward(100)
+  t.left(63)
